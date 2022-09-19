dfda5dc0bf3bdc92182b889969c3776ef3015458
Tulio-Viana <t.viana2@icloud.com>
2022-09-19 15:11:39 -0300

    Calendario sem cadastrar no banco

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:idosos/modules/pages/profile/utils/user_preferences.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'Core/App.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -12,5 +13,5 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   await UserPreferences.init();
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
